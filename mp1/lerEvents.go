0de0a5f7a782dcf4e68091d727a09a1f152dcc96
0xHEXNUMBERS <0xhexnumbers@gmail.com>
2021-08-23 04:40:15 -0500

    add documentation to lerEvents

--- mp1/lerEvents.go
+++ mp1/lerEvents.go
@@ -1,5 +1,7 @@
 package mp1
 
+//lerRedFork handles the RBR fork in the middle of the board when the red
+//gates are down.
 type lerRedFork struct {
 	Player int
 	Moves  int
@@ -10,6 +12,7 @@ var lerRedForkDestinations = []Response{
 	ChainSpace{11, 0},
 }
 
+//Responses returns the 2 destinations from the fork.
 func (l lerRedFork) Responses() []Response {
 	return lerRedForkDestinations
 }
@@ -18,17 +21,20 @@ func (l lerRedFork) ControllingPlayer() int {
 	return l.Player
 }
 
+//Handle moves the player to the ChainSpace r.
 func (l lerRedFork) Handle(r Response, g *Game) {
 	dest := r.(ChainSpace)
 	g.Players[l.Player].CurrentSpace = dest
 	g.MovePlayer(l.Player, l.Moves-1)
 }
 
+//lerRobot let's the player decide to either pay and raise/lower gates or ignore the robot.
 type lerRobot struct {
 	Player int
 	Moves  int
 }
 
+//Responses returns a slice of bools (true/false).
 func (l lerRobot) Responses() []Response {
 	return []Response{true, false}
 }
@@ -37,6 +43,7 @@ func (l lerRobot) ControllingPlayer() int {
 	return l.Player
 }
 
+//Handle pays the robot 20 coins and switches the gates only if r is true.
 func (l lerRobot) Handle(r Response, g *Game) {
 	pay := r.(bool)
 	if pay {
